afc8598555639bc5052735b976b866501997ffde
Shawn Plummer <smplummer@gmail.com>
2013-04-09 14:08:07 -0400

    updated with stuff

--- aliases.zsh
+++ aliases.zsh
@@ -8,3 +8,7 @@ alias sl='ls $LS_OPTIONS' # often screw this up
 
 # Set vi to open vim for systems that have both
 vi='vim'
+
+# Quick way to rebuild the Launch Services database and get rid
+# of duplicates in the Open With submenu.
+alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
