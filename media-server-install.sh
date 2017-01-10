fdec4ebde5735fd84bd461034414a37e92eaaccc
Shawn Plummer <smplummer@gmail.com>
2017-01-09 20:18:30 -0500

    added comman line tools

--- media-server-install.sh
+++ media-server-install.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# add some taps
+brew tap caskroom/fonts
+
+# Install some command lines tools
+brew install coreutils
+brew install python3
+brew install git
+brew install libdvdcss
+brew install mas
+brew install ffmpeg
+
+# some fonts
+brew cask install font-inconsolata
+brew cask install font-source-code-pro
 
 brew cask install handbrake
 brew cask install vlc
@@ -15,3 +29,4 @@ brew cask install makemkv
 brew cask install 1password
 brew cask install the-unarchiver
 brew cask install macpar-deluxe
+brew cask install visual-studio-code
