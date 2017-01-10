745312454984464fa40fa05cf54fc8e69828208e
Shawn Plummer <smplummer@gmail.com>
2017-01-09 20:06:00 -0500

    added file for media server install

---
+++ media-server-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+brew cask install handbrake
+brew cask install vlc
+brew cask install utorrent
+brew cask install plex-media-server
+brew cask install sonarr
+brew cask install sabnzbd
+brew cask install couchpotato
+brew cask install alfred
+brew cask install bbedit
+brew cask install istat-menus
+brew cask install makemkv
+brew cask install 1password
+brew cask install the-unarchiver
+brew cask install macpar-deluxe
